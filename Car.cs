5aeefd1bb2d0e841d5a9ba533f786740c41463bf
Poul Henriksen <POHE@zealand.dk>
2024-09-24 10:47:15 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,45 @@
+﻿
+/// <summary>
+/// A very simple representation of a car
+/// </summary>
+public class Car
+{
+    #region Instance fields
+    private string _licensePlate;
+    private int _price;
+    private static int _numberOfObjects =0;
+    private static int _usesLicensePlate =0;
+    #endregion
+
+    #region Constructor
+    public Car(string licensePlate, int price)
+    {
+        _numberOfObjects++;
+        _licensePlate = licensePlate;
+        _price = price;
+    }
+    #endregion
+
+    #region Properties
+    public string LicensePlate
+    {
+        get {
+            _usesLicensePlate++;
+            return _licensePlate; 
+        }
+        set { _licensePlate = value; }
+    }
+
+    public int Price
+    {
+        get { return _price; }
+        set { _price = value; }
+    }
+    #endregion
+
+    public static void PrintUsageStatistics()
+    {
+        Console.WriteLine($"NumberOfObjects {_numberOfObjects}");
+        Console.WriteLine($"Uses of LicencePate {_usesLicensePlate}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+// ListMethods test
+List<int> aList = new List<int> { 23, 86, 51, 11, 39 };
+//ListMethods listMethodsObject = new ListMethods();
+
+int smallest = ListMethods.FindSmallestNumber(aList);
+Console.WriteLine($"The smallest number in the list is : {smallest}");
+
+int average = ListMethods.FindAverage(aList);
+Console.WriteLine($"The average of the list is : {average}");
+
+Car c1 = new Car("121212", 1200);
+Car c2 = new Car("232323", 3400);
+string c1LicensePLate = c1.LicensePlate;
+Car.PrintUsageStatistics();
+
